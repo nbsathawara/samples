8e008ffde0383052385f86a45b0f05071c79cd6a
NIKHIL-ACTABL <nikhil.sathawara@actabl.com>
2023-07-17 13:07:25 +0530

    lession 2

--- app/src/main/java/com/example/demoproject/LeagueActivity.kt
+++ app/src/main/java/com/example/demoproject/LeagueActivity.kt
@@ -1,14 +1,11 @@
 package com.example.demoproject
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-
-class WelcomeActivity : AppCompatActivity() {
-
+class LeagueActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+        setContentView(R.layout.activity_league)
     }
 }
