f177f1a91d483a28f229e0b42349869a0dc2c7ff
NIKHIL-ACTABL <nikhil.sathawara@actabl.com>
2023-07-17 14:22:52 +0530

    lession3

--- app/src/main/java/com/example/demoproject/WelcomeActivity.kt
+++ app/src/main/java/com/example/demoproject/WelcomeActivity.kt
@@ -3,6 +3,7 @@ package com.example.demoproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcome.*
 
 
 class WelcomeActivity : AppCompatActivity() {
@@ -10,5 +11,10 @@ class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        btnGetStarted.setOnClickListener {
+            val intent = Intent(this, LeagueActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
