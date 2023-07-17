508e27bcac8e14b9572435214c28b722ecad9089
NIKHIL-ACTABL <nikhil.sathawara@actabl.com>
2023-07-17 12:09:26 +0530

    Intital Commit

---
+++ app/src/main/java/com/example/demoproject/WelcomeActivity.kt
@@ -0,0 +1,14 @@
+package com.example.demoproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class WelcomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
