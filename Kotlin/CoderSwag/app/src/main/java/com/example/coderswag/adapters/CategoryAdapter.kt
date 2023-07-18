51d6f02356dfb44b4565cbb14f1f51b0ad888adc
NIKHIL-ACTABL <nikhil.sathawara@actabl.com>
2023-07-18 12:02:11 +0530

    lession1

---
+++ Kotlin/CoderSwag/app/src/main/java/com/example/coderswag/adapters/CategoryAdapter.kt
@@ -0,0 +1,61 @@
+package com.example.coderswag.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.coderswag.R
+import com.example.coderswag.databinding.CellCatListBinding
+import com.example.coderswag.model.Category
+import com.example.coderswag.serives.DataService
+
+class CategoryAdapter(context: Context, categories: List<Category>) : BaseAdapter() {
+
+    val context = context
+    val categories = categories
+
+
+    override fun getCount(): Int {
+        return categories.count()
+    }
+
+    override fun getItem(position: Int): Any {
+        return categories.get(position)
+    }
+
+    override fun getItemId(position: Int): Long {
+        return 0
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        var catView: View
+        var holder = ViewHolder()
+        if (convertView == null) { 
+            catView = LayoutInflater.from(context).inflate(R.layout.cell_cat_list, null)
+            holder.catImag = catView.findViewById(R.id.imgCat)
+            holder.nameCat = catView.findViewById(R.id.nameCat)
+            catView.tag = holder
+        } else {
+            holder = convertView?.tag as ViewHolder
+            catView = convertView
+        }
+        val category = categories[position]
+        holder.nameCat?.text = category.title
+        holder.catImag?.setImageResource(
+            context.resources.getIdentifier(
+                category.image,
+                "drawable", context.packageName
+            )
+        )
+        return catView
+    }
+
+    private class ViewHolder() {
+        var catImag: ImageView? = null
+        var nameCat: TextView? = null
+    }
+}
--- Kotlin/CoderSwag/app/src/main/java/com/example/coderswag/controller/MainActivity.kt
+++ Kotlin/CoderSwag/app/src/main/java/com/example/coderswag/controller/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.coderswag.controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.example.coderswag.R
+import com.example.coderswag.adapters.CategoryAdapter
+import com.example.coderswag.databinding.ActivityMainBinding
+import com.example.coderswag.model.Category
+import com.example.coderswag.serives.DataService
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var adapter: CategoryAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initialize()
+    }
+
+    fun initialize() {
+        adapter = CategoryAdapter(this, DataService.categories)
+        binding.catListView.adapter = adapter
     }
 }
