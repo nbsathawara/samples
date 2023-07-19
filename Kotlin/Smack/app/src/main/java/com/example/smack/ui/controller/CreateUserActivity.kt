8ffb3b8038cf0648542f8e36d635f30c5ddd36ed
NIKHIL-ACTABL <nikhil.sathawara@actabl.com>
2023-07-19 11:12:57 +0530

    Smack app create use profile and background color functionality added.

--- Kotlin/Smack/app/src/main/java/com/example/smack/ui/controller/CreateUserActivity.kt
+++ Kotlin/Smack/app/src/main/java/com/example/smack/ui/controller/CreateUserActivity.kt
@@ -1,25 +1,51 @@
 package com.example.smack.ui.controller
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.smack.databinding.ActivityCreateUserBinding
-import com.example.smack.databinding.ActivityLogInBinding
+import java.util.Random
 
 class CreateUserActivity : AppCompatActivity() {
+
     lateinit var binding: ActivityCreateUserBinding
+    val random = Random()
+    var userAvatar = "profileDefault"
+    var avtarColor = "[05,0.5,0.5,1]"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCreateUserBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 
-    fun userProfileImgClicked(view: View) {
+    fun generateAvatarClicked(view: View) {
+        val color = random.nextInt(2)
+        val avatarIndex = random.nextInt(28)
+
+        if (color == 0)
+            userAvatar = "light$avatarIndex"
+        else
+            userAvatar = "dark$avatarIndex"
+
+        binding.imgProfile.setImageResource(
+            resources.getIdentifier(
+                userAvatar,
+                "drawable",
+                packageName
+            )
+        )
 
     }
 
     fun generateColorClicked(view: View) {
+        val r = random.nextInt(255)
+        val g = random.nextInt(255)
+        val b = random.nextInt(255)
 
+        avtarColor = "[${r.toDouble() / 255},${g.toDouble() / 255},${b.toDouble() / 255},1]"
+        binding.imgProfile.setBackgroundColor(Color.rgb(r, g, b))
     }
 
 
