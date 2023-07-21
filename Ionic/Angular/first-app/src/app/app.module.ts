5ea7816124b056247e6d5f8bb3385e1a02eadb36
NIKHIL-ACTABL <nikhil.sathawara@actabl.com>
2023-07-21 21:51:35 +0530

    Angular basic app added.

---
+++ Ionic/Angular/first-app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { PersonComponent } from './person/persons.component';
+import { PersonInputComponent } from './person/person-input.component';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PersonComponent,
+    PersonInputComponent
+  ],
+  imports: [
+    BrowserModule, FormsModule, AppRoutingModule, HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Ionic/first-app/src/app/home/home.page.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: true,
+  imports: [IonicModule],
+})
+export class HomePage {
+  constructor() {}
+text='Starting Text!!'
+
+  onchangeText(){
+this.text='Changed!!!!'
+  }
+}
