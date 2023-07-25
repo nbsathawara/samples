d51dd1631c6ad998cee05d83e02536219f4a21a3
NIKHIL-ACTABL <nikhil.sathawara@actabl.com>
2023-07-25 18:57:55 +0530

    Rent a Home initial project added

---
+++ Ionic/app_rent-your-home/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Rent Your Home',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
