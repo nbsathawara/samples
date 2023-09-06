fc95d81627aed475d641c87a3c2786b95cb345e4
NIKHIL-ACTABL <nikhil.sathawara@actabl.com>
2023-09-06 14:04:38 +0530

    Xamarin XAML & Layoit samples added.

---
+++ Xamarin/1. XAML/HelloWorld/HelloWorld/HelloWorld/QuotesPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HelloWorld
+{
+    public partial class QuotesPage : ContentPage
+    {
+        Random random = new Random();
+        public QuotesPage()
+        {
+            InitializeComponent(); 
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            lblQuote.Text = "Quote " + random.Next(101);
+        }
+    }
+}
+
---
+++ Xamarin/2. Layouts/Layouts/Layouts/Layouts/GridPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Layouts
+{	
+	public partial class GridPage : ContentPage
+	{	
+		public GridPage ()
+		{
+			InitializeComponent ();
+		}
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+			string btnText= (sender as Button).Text;
+			int n;
+			if (int.TryParse(btnText, out n))
+				lblNumber.Text = lblNumber.Text + btnText;
+			else
+				lblNumber.Text = lblNumber.Text.Length>0 ? lblNumber.Text.Substring(0, lblNumber.Text.Length - 1)
+					: "";
+        }
+    }
+}
+
