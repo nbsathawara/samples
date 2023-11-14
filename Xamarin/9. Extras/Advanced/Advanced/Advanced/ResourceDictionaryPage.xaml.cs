55400f8226fd4998a364fb5e9e4928dda6f88726
NIKHIL-ACTABL <nikhil.sathawara@actabl.com>
2023-11-14 12:39:44 +0530

    Xamarin advanced samples added

---
+++ Xamarin/9. Extras/Advanced/Advanced/Advanced/ResourceDictionaryPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Advanced
+{	
+	public partial class ResourceDictionaryPage : ContentPage
+	{	
+		public ResourceDictionaryPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
