a94cbc996e49ee401828de26a0ce2f03dd761a01
NIKHIL-ACTABL <nikhil.sathawara@actabl.com>
2023-11-15 13:21:39 +0530

    MAUI carlist app initial project added

---
+++ MAUI/1. Car Listing App/Car Listing/Car Listing/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Car_Listing;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ MAUI/1. Car Listing App/Car Listing/Car Listing/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Car_Listing;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
